e85ec814f27c16cf3cb43a7516c5b63233e05c31
camilosanchezz077-spec <camilosanchezz077@gmail.com>
2026-09-02 23:44:55 -0500

    Update TipoMuestra.php
    parte de tipoMuestra biomuestra

---
+++ classes/TipoMuestra.php
@@ -0,0 +1,18 @@
+<?php
+class TipoMuestra {
+    private int $id;
+    private string $nombre;
+
+    public function __construct(int $id, string $nombre) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getNombre(): string {
+        return $this->nombre;
+    }
+}
